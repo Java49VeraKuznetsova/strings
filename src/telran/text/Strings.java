4b1369718d9f56ee5cd35efc0e829d9de95437e4
Vera Kuznetsova <kuznetsova-vera@yandex.ru>
2023-06-26 18:17:12 +0300

    HW 24(75) done

---
+++ src/telran/text/Strings.java
@@ -0,0 +1,27 @@
+package telran.text;
+
+
+public class Strings {
+	public static String javaVariableName() {
+		
+		return "[a-zA-Z$][\\w$]*|_[\\w$]+";
+	}
+	public static String zero_300() {
+		return "[1-9]\\d?|[1-2]\\d\\d|300|0";
+	}
+	public static String ipV4Octet() {
+		
+		// positive number from 0 to 255 and leading zeros are aloowed
+		return "\\d\\d?|[0-1]\\d\\d|2[0-4]\\d|25[0-5]";
+	}
+	public static String ipV4() {
+		
+		// four ipV4 octets separated by dot 123.123.255.01
+		String point = "\\.";
+	
+		String octet = "("+ipV4Octet()+")";
+	
+		return "("+octet+"\\."+")"+"{3}"+octet;
+	}
+
+}
---
+++ src/telran/text/StringsTest.java
@@ -0,0 +1,119 @@
+package telran.text;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StringsTest {
+
+	@Test
+	void test() {
+	//String regex = "gray|grey|griy";
+		//String regex = "gra|e|iy";
+		/*
+		String regex = "gr(a|e|i)y";
+		assertTrue("gray".matches(regex));
+		assertTrue("grey".matches(regex));
+		assertTrue("griy".matches(regex));
+		assertFalse("groy".matches(regex));
+		*/
+		/*
+		String regex = "a?1234";
+		assertTrue("1234".matches(regex));
+		assertTrue("a1234".matches(regex));
+		assertFalse("b1234".matches(regex));
+		assertFalse("aa1234".matches(regex));
+		*/
+		/*
+		String regex = "a*1234";
+		assertTrue("1234".matches(regex));
+		assertTrue("a1234".matches(regex));
+		assertFalse("b1234".matches(regex));
+		assertTrue("aa1234".matches(regex));
+		*/
+		/*
+		String regex = "a+1234";
+		assertFalse("1234".matches(regex));
+		assertTrue("a1234".matches(regex));
+		assertFalse("b1234".matches(regex));
+		assertTrue("aa1234".matches(regex));
+		*/
+		
+		String regex = "(a|b)+1234";
+		assertFalse("1234".matches(regex));
+		assertTrue("a1234".matches(regex));
+		assertTrue("b1234".matches(regex));
+		assertTrue("aa1234".matches(regex));
+		
+	}
+	@Test
+	void javaVariableNameTest() {
+		String regex = Strings.javaVariableName();
+		assertTrue("a".matches(regex));
+		assertTrue("$".matches(regex));
+		assertTrue("$2".matches(regex));
+		assertTrue("$_".matches(regex));
+		assertTrue("__".matches(regex));
+		assertTrue("_2".matches(regex));
+		assertTrue("a_b".matches(regex));
+		assertTrue("A_B".matches(regex));
+		assertTrue("abc1234567890000".matches(regex));
+		assertFalse("1a".matches(regex));
+		assertFalse("_".matches(regex));
+		assertFalse("a#".matches(regex));
+		assertFalse("a b".matches(regex));
+		assertFalse("a-b".matches(regex));
+		assertFalse(" ab".matches(regex));
+	}
+	@Test
+	void zero_300Test() {
+		String regex = Strings.zero_300();
+		assertTrue("0".matches(regex));
+		assertTrue("9".matches(regex));
+		assertTrue("299".matches(regex));
+		assertTrue("300".matches(regex));
+		assertTrue("99".matches(regex));
+		assertFalse("01".matches(regex));
+		assertFalse("00".matches(regex));
+		assertFalse("1111".matches(regex));
+		assertFalse("301".matches(regex));
+		assertFalse("3000".matches(regex));
+		assertFalse("310".matches(regex));
+		assertFalse("-1".matches(regex));
+		assertFalse("3 ".matches(regex));
+	}
+	@Test
+	void ipV4OctetTest () {
+		String regex = Strings.ipV4Octet();
+		assertTrue("000".matches(regex));
+		assertTrue("00".matches(regex));
+		assertTrue("0".matches(regex));
+		assertTrue("99".matches(regex));
+		assertTrue("1".matches(regex));
+		assertTrue("10".matches(regex));
+		assertTrue("199".matches(regex));
+		assertTrue("200".matches(regex));
+		assertTrue("249".matches(regex));
+		assertTrue("250".matches(regex));
+		assertTrue("255".matches(regex));
+		assertFalse("0000".matches(regex));
+		assertFalse(" 1".matches(regex));
+		assertFalse(".0".matches(regex));
+		assertFalse("-1".matches(regex));
+		assertFalse("256".matches(regex));
+		assertFalse("1000".matches(regex));
+	}
+	@Test
+	void IpV4Test() {
+		String regex = Strings.ipV4();
+		assertTrue("0.0.0.0".matches(regex));
+		assertTrue("1.1.1.1".matches(regex));
+		assertTrue("99.99.12.09".matches(regex));
+		assertTrue("100.199.200.255".matches(regex));
+		assertFalse(".1.2.3.4".matches(regex));
+		assertFalse("1.2.3.4.".matches(regex));
+		assertFalse("1.2.3".matches(regex));
+		assertFalse("1.2.3.4.5".matches(regex));
+		assertFalse("1.2.&3.4.".matches(regex));
+	}
+}
